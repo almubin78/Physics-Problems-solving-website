c9af9745137556423adbc734d940db28b42c0f2e
Al Mubin <almubin78@gmail.com>
2025-02-12 21:00:20 +0600

    physics solver for motion add to component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import { useState } from 'react';
 import './App.css';
+import PhysicsCalculator from './Motion/PhysicsCalculator';
 
 
 function App() {
-  const [variableToSolve, setVariableToSolve] = useState('');
-  const [selectedLaw,setSelectedLaw] = useState('');
-  const [inputValues,setInputValues] = useState({});
-  const [result,setResult] = useState(null);
-
-  const variables = ['s','u','v','a','t'];
-  const laws = {
-    s:[
-      {formula:'s = ut + 1/2at^2',variablesNeed:['u','t','a']},
-      {formula:'s = vt',variablesNeed:['v','t']},
-    ]
-  }
-  const handleVariableChange = (variable) => {
-    console.log(variable);
-    setVariableToSolve(variable);
-  }
+  
 
   return (
     <div className="App">
-      this is calculator app
+      <PhysicsCalculator/>
     </div>
   );
 }
