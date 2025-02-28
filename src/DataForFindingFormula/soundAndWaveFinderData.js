4bb6a89fb61dc3130b7727e4a00d4de439b8d3ed
Al Mubin <almubin78@gmail.com>
2025-02-28 08:13:06 +0600

    sound and wave laws finder data proccesing

--- src/DataForFindingFormula/soundAndWaveFinderData.js
+++ src/DataForFindingFormula/soundAndWaveFinderData.js
@@ -1,8 +1,9 @@
 export const soundAndWaveFinderData = [
     // { formula: 'v = fλ', required: ['f', 'λ'], compute: (vals) => (vals.f * vals.λ) },
     { formula: 'v = fλ', required: ['তরংগের_কম্পাংক', 'তরংগদৈর্ঘ্য'], compute: (vals) => (vals.তরংগের_কম্পাংক * vals.তরংগদৈর্ঘ্য) },
-    { formula: 'v = λ/T', required: ['λ','T'],exclude:['a'] ,compute: (vals) => (vals.λ/vals.T) },
-    { formula: 'a = F/m', required: ['F','m'],exclude:[] ,compute: (vals) => (vals.F /vals.m) },
-    { formula: 'a = (v-u)/t', required: ['v','u','t'],exclude:[''] ,compute: (vals) => (vals.F /vals.m) },
+    { formula: 'v = λ/T', required: ['তরংগদৈর্ঘ্য','তরংগের_পর্যায়কাল'],exclude:['a'] ,compute: (vals) => (vals.তরংগদৈর্ঘ্য/vals.তরংগের_পর্যায়কাল) },
+    { formula: 'λ=v/T', required: ['তরংগের_বেগ','তরংগের_পর্যায়কাল'],exclude:[] ,compute: (vals) => (vals.তরংগের_বেগ /vals.তরংগের_পর্যায়কাল) },
+    { formula: 'f = 1/T', required: ['তরংগের_পর্যায়কাল'],exclude:[''] ,compute: (vals) => (1 /vals.তরংগের_পর্যায়কাল) },
+    { formula: 'T = 1/f', required: ['তরংগের_কম্পাংক'],exclude:[''] ,compute: (vals) => (1 /vals.তরংগের_কম্পাংক) },
     
   ];
