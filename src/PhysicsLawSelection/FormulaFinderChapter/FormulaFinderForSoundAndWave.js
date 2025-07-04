9306292e1c1c008278a1eebccf2ccb7f92a184a3
almubin78 <almubin78@gmail.com>
2025-07-04 23:18:46 +0600

    Reflector and Distance Laws finding coding start

--- src/PhysicsLawSelection/FormulaFinderChapter/FormulaFinderForSoundAndWave.js
+++ src/PhysicsLawSelection/FormulaFinderChapter/FormulaFinderForSoundAndWave.js
@@ -3,7 +3,7 @@ import FormulaFinder from '../CommonComponent/FormulaFinder';
 import { soundAndWaveFinderData } from '../../DataForFindingFormula/soundAndWaveFinderData';
 
 const FormulaFinderForSoundAndWave = () => {
-    const variables =['তরংগের_বেগ','তরংগের_কম্পাংক','তরংগের_পর্যায়কাল','তরংগদৈর্ঘ্য','বায়ুর_তাপমাত্রা','কেলভিন_তাপমাত্রায়_শব্দের_বেগ']
+    const variables =['তরংগের_বেগ','তরংগের_কম্পাংক','তরংগের_পর্যায়কাল','তরংগদৈর্ঘ্য','বায়ুর_তাপমাত্রা','কেলভিন_তাপমাত্রায়_শব্দের_বেগ','প্রতিফলকের_দূরত্ব','প্রতিফলকের_ন্যুনতম_দুরত্ব']
     return (
         <FormulaFinder
             title='শব্দ ও তরঙ্গ- অধ্যায়ের সূত্র খোঁজ'
